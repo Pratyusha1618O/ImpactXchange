72c5c569a8c41f24a8bd4d0e0f77e18334e922bb
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-04-30 00:07:45 +0530

    NGO logout

---
+++ ngo-logout.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+include("./server/config.php");
+$email = $_SESSION['ngo-email'];
+
+$sql = "DELETE FROM ngo_login  WHERE ngo_email = '$email' ";
+mysqli_query($dbcon, $sql);
+
+
+session_unset();
+session_destroy();
+header("Location: index.php");
+
+
+
+exit();
+
+?>
